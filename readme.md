58e17a8ef7cd2b1a5ab695f228f6cef80696ea4b
Prayag Patel <prayagpatel283@gmail.com>
2025-10-03 17:03:53 -0400

    Updated the script to make the cpu usage value update in it's specific location. Added a LICENSE.txt to indicate that this project is under the MIT License. Updated the readme.md file to describe what the project is about, current features, future features that neds to be implemented, the command used to quit the script, and the current problems in the project.

--- readme.md
+++ readme.md
@@ -1,3 +1,40 @@
 # Bash System Monitoring
 
-This is a simple bash script that shows your systems information such as the cpu, disk, and network information.
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](LICENSE)
+![Language](https://img.shields.io/badge/language-bash-blue.svg)
+
+A bash script that displays your systems cpu, disk, and network information in a table style. Here is a screenshot of how the table will look like,
+
+![alt text](image.png)
+
+# Commands:
+
+- q - quit the script.
+
+# Current Features:
+
+1. Displays cpu, disk, and network information on a terminal in a table style. $\textbf{The cpu usage doesn't reflect the built in systems task manager or any similar programs}$.
+
+2. Updates only the cpu usage value in it's specific column $\textit{only}$ when the terminal window size is in full screen.
+
+3. Coded entirely in bash.
+
+# Future Features:
+
+1. Updates the cpu ussage value and any other values that can be updated will be done in it's own colum and row, even when the terminal window has been resized.
+
+2. Create a status column for the cpu information and networking information to see if the cpu and network works properly and either display "Good" or "Bad" in green and red respectively.
+
+3. Update the design of the table to have a box surrounding the data to make it look polished.
+
+# Problems:
+
+1. The cpu usage doesn't update in it's column and row when resizing the terminal window. It only udpates when the terminal window is the full size.
+
+2. The cursor doesn't properly go back to the original position to type in "q" to quit the script.
+
+3. Don't have a way to see how many rows and columns does the fixed table take up in the terminal to dynamically get the location of values that can get updated in "real time".
+
+# Credit
+
+Lead Developer - Prayag Patel
--- system_monitor.sh
+++ system_monitor.sh
@@ -1,71 +1,109 @@
 #!/bin/bash 
 
-echo "=== System Health Dashboard ==="
-
-echo 
-
-
 # Constant Variables
 HOSTNAME=$(hostname)
 USER=$(whoami)
 
 # CPU Information Variables
-CPU_NAME=$(lscpu | grep "Model name" | awk -F: '{print $2}' | xargs)
+CPU_NAME="Intel i5" # Temp, find out a way to get the cpu name of the system and shorten it. 
 CPU_CORES=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
-PREV_IDLE=""
-PREV_TOTAL=""
 
-# Network Information Varibales
+# Loop Varibale
+flag=true
+
+# Functions
+function get_cpu_usage(){
+    # Get the cpu information need to determine cpu usage.
+    read -r cpu user nice system idle iowait irq softirq steal guest guest_nice < /proc/stat 
+    local total_cpu_time_1=$((user + nice + system + idle + iowait + irq + softirq + steal + guest + guest_nice)) 
+    local cpu_idle_time_1=$((idle + iowait))
+
+    sleep 0
+
+    read -r cpu user nice system idle iowait irq softirq steal guest guest_nice < /proc/stat 
+    local total_cpu_time_2=$((user + nice + system + idle + iowait + irq + softirq + steal + guest + guest_nice))
+    local cpu_idle_time_2=$((idle + iowait))
+
+    # Calculate the difference.
+    local total_difference=$((total_cpu_time_2 - total_cpu_time_1))
+    local idle_difference=$((cpu_idle_time_2 - cpu_idle_time_1))
+
+    if [[ $idle_difference -eq 0 ]]; then 
+        usage=0
+        echo -n "$usage"
+    fi
+
+    usage=$((100 * (total_difference - idle_difference) / total_difference))  
+
+    echo -n "$usage"
+}
+
+echo 
+
+echo "General System Information" 
+echo "-------------------------------------------------"
+printf "%-20s %10s %10s\n" "Hostname" "User" "OS"
+echo "-------------------------------------------------"
+printf "%-20s %10s %10s\n" "$HOSTNAME" "$USER" "$OSTYPE"
+echo "-------------------------------------------------"
+
+echo 
+
+echo "CPU Information" 
+echo "-----------------------------------------------"
+printf "%-20s %10s %10s\n" "CPU Name" "Cores" "Usage%"
+echo "-----------------------------------------------"
+printf "%-20s %10s %10s\n" "$CPU_NAME" "$CPU_CORES" "0%"
+echo "-----------------------------------------------"
+
+echo
+
+echo "Disk Information"
+echo "-----------------------------------------------------------------------"
+printf "%-20s %10s %10s %10s %10s\n" "Device" "Mounted" "Used" "Free" "Usage%" 
+echo "-----------------------------------------------------------------------"
+
+echo 
+
+df -h | grep "^/dev" | awk '{printf "%-20s %10s %10s %10s %10s\n", $1, $6, $3, $4, $5}'
+
+echo "-----------------------------------------------------------------------"
+
+echo 
+
+echo "Network Infromation"
+echo "------------------------------------------------------------------------------"
+printf "%-10s %10s %20s %20s\n" "RX (MB)" "TX (MB)" "Active Connection" "Listening Services"
+echo "------------------------------------------------------------------------------"
+printf "%-10s %10s %20s %20s\n" "0" "0" "0" "0"
+echo "------------------------------------------------------------------------------"
+
+# Save the original cursor
+tput sc
+
+# Save the coordinates of the cpu usage information and network RT/TX information.
+cpu_usage_col=$((39))
+cpu_usage_row=$(($(tput lines) - 34))
 
 
-user_input=""
+while true; do
+    # Updating cpu usage infromation
+    usage=$(get_cpu_usage)
 
+    tput civis
+    tput rc
+    tput cup $cpu_usage_row $cpu_usage_col
+    printf "%-4s" "${usage}%"
 
+    tput cup 47 0
+    tput cnorm
 
-while [ "$user_input" != "quit" ]; do 
-    read -p ">> " user_input
+    sleep 1
 
-    if [ "$user_input" == "run" ]; then
+    read -t 1 -p "" user
+    if [[ $user == "q" ]]; then
         echo 
-
-        echo "General System Information" 
-        echo "-------------------------------------------------"
-        printf "%-20s %10s %10s\n" "Hostname" "User" "OS"
-        echo "-------------------------------------------------"
-        printf "%-20s %10s %10s\n" "$HOSTNAME" "$USER" "$OSTYPE"
-        echo "-------------------------------------------------"
-
-        echo 
-
-        echo "CPU Information" 
-        echo "---------------------------------------------------------------------------"
-        printf "%-20s %10s %10s %10s %10s\n" "CPU Name" "Cores" "Usage%" "Top Proc" "Temp"
-        echo "---------------------------------------------------------------------------"
-
-        echo
-
-        echo "Disk Information"
-        echo "-----------------------------------------------------------------------"
-        printf "%-20s %10s %10s %10s %10s\n" "Device" "Mounted" "Used" "Free" "Usage%" 
-        echo "-----------------------------------------------------------------------"
-
-        echo 
-
-        df -h | grep "^/dev" | awk '{printf "%-20s %10s %10s %10s %10s\n", $1, $6, $3, $4, $5}'
-
-        echo "-----------------------------------------------------------------------"
-
-        echo 
-
-        echo "Network Infromation"
-        ESTABLISHED=$(ss -t | grep ESTAB | wc -l)
-        LISTENING=$(ss -tln | grep LISTEN | wc -l)
-        echo "-------------------------------------------------------------------------------------"
-        printf "%-10s %10s %20s %20s\n" "RX (MB)" "TX (MB)" "Active Connection" "Listening Services"
-        echo "-------------------------------------------------------------------------------------"
-        cat /proc/net/dev | grep eth0 | awk '{printf "%-10.2f %10.2f ", $2/1024/2024, $10/1024/1024}'
-        printf "%20s %20s\n" "$ESTABLISHED" "$LISTENING"
-        echo "-------------------------------------------------------------------------------------"
-
-    fi 
-done 
+        echo "Exiting ..."
+        break
+    fi
+done
